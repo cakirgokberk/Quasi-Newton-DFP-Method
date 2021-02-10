752042cb9e4a87fe5e710c4310a7445b0926d63c
Gökberk Çakıroğlu <71937484+cakirgokberk@users.noreply.github.com>
2021-02-10 17:51:15 +0300

    Update Main.m

--- Main.m
+++ Main.m
@@ -3,9 +3,7 @@
 % 27/12/2020
 % DFP main File
  
-
+% [1,2] --> starting point
 DFP('myfunc',[1 2],25, 0.00001, 0,1 ,0);
 
 
-%%%     myfunc.m dosyasından fonksiyonu degistirebilirsiniz.
-%%%     Buradan da baslangıc noktasını degistirebilirsiniz.
